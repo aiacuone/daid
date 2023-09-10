babff9ccf2c6a145ef7742af3985e9810eedfc39
aiacuone <age.iacuone@aventus.io>
2023-09-10 13:11:00 +0200

    routing with lorem text

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,27 +1,26 @@
 <div class="h-screen w-screen stack bg-gray-100">
 	<!-- HEADER -->
 	<div class="w-full bg-gray-500 hstack p-2">
-		<div class="bg-gray-100 rounded-md">
-			<p class="h-10 w-20 center">Logo</p>
+		<div class=" flex-1 sm:flex-none center">
+			<a href="/" class="h-10 w-20 bg-gray-100 rounded-md center">Logo</a>
 		</div>
-		<div class="flex-1" />
-		<div class="hstack gap-2">
-			<p class="center bg-gray-100 py-2 px-8 rounded-md">Link</p>
-			<p class="center bg-gray-100 py-2 px-8 rounded-md">Link</p>
-			<p class="center bg-gray-100 py-2 px-8 rounded-md">Link</p>
-			<p class="center bg-gray-100 py-2 px-8 rounded-md">Link</p>
+		<div class="flex-1 hidden sm:flex" />
+		<div class="gap-2 hidden sm:flex">
+			<a href="/about" class="center bg-gray-100 py-2 px-8 rounded-md">About</a>
+			<a href="/services" class="center bg-gray-100 py-2 px-8 rounded-md">Services</a>
+			<a href="/contact" class="center bg-gray-100 py-2 px-8 rounded-md">Contact</a>
 		</div>
 	</div>
-	<div class="flex-1">
+	<div class="flex-1 center p-4">
 		<slot />
 	</div>
 	<!-- FOOTER -->
 	<div class="w-full bg-gray-500 hstack p-2">
 		<div class="hstack gap-2">
-			<p class="center bg-gray-100 p-2 rounded-md">Social Link</p>
-			<p class="center bg-gray-100 p-2 rounded-md">Social Link</p>
-			<p class="center bg-gray-100 p-2 rounded-md">Social Link</p>
-			<p class="center bg-gray-100 p-2 rounded-md">Social Link</p>
+			<p class="center bg-gray-100 p-2 rounded-md">Social</p>
+			<p class="center bg-gray-100 p-2 rounded-md">Social</p>
+			<p class="center bg-gray-100 p-2 rounded-md">Social</p>
+			<p class="center bg-gray-100 p-2 rounded-md">Social</p>
 		</div>
 	</div>
 </div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,4 +2,11 @@
 	import '../global.css';
 </script>
 
-<h1 class="text-5xl">this is daid</h1>
+<div class="stack center gap-3">
+	<h1 class="text-4xl">:DAID</h1>
+	<div class="flex flex-col sm:flex-row gap-2">
+		<a href="/about" class="bg-gray-300 py-2 px-8 rounded-md">About</a>
+		<a href="/services" class="bg-gray-300 py-2 px-8 rounded-md">Services</a>
+		<a href="/contact" class="bg-gray-300 py-2 px-8 rounded-md">Contact</a>
+	</div>
+</div>
