238e761c1d68d07be455236a722ecaedd7bde00d
aiacuone <age.iacuone@aventus.io>
2023-09-11 13:11:44 +0200

    mobile scroll issue

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,36 +6,34 @@
 	import '../global.css';
 </script>
 
-<div class="h-screen w-screen bg-gray-100">
-	<div class="h-full w-full stack">
-		<!-- HEADER -->
-		<div class="w-full hstack py-2 px-10 header">
-			<div class=" flex-1 sm:flex-none center">
-				<a href="/" class="rounded-md center"><DaidLight height="40px" /></a>
-			</div>
-			<div class="flex-1 hidden sm:flex" />
-			<div class="gap-6 hidden sm:flex">
-				{#each localLinks as link}
-					<a href={link.path} class="text-lg text-white center uppercase">{link.label}</a>
-				{/each}
-			</div>
+<div class="h-screen w-screen bg-gray-100 stack" style={'max-height:dvh'}>
+	<!-- HEADER -->
+	<div class="w-full hstack py-2 px-10 header">
+		<div class=" flex-1 sm:flex-none center">
+			<a href="/" class="rounded-md center"><DaidLight height="40px" /></a>
 		</div>
-		<div class="flex-1 center p-4">
-			<slot />
+		<div class="flex-1 hidden sm:flex" />
+		<div class="gap-6 hidden sm:flex">
+			{#each localLinks as link}
+				<a href={link.path} class="text-lg text-white center uppercase">{link.label}</a>
+			{/each}
 		</div>
-		<!-- FOOTER -->
-		<div class="w-full hstack py-2 px-10 justify-center sm:justify-start footer">
-			<div class="hstack gap-5">
-				<a href="https://www.linkedin.com/in/dahyad/?originalSubdomain=uk" target="_blank">
-					<Icon icon={icons.linkedin} height="30px" />
-				</a>
-				<a href="https://www.instagram.com/dipi_d/?hl=en" target="_blank">
-					<Icon icon={icons.instagram} height="30px" />
-				</a>
-				<a href="https://www.tiktok.com/@dipzlikechipz_" target="_blank">
-					<Icon icon={icons.tiktok} height="30px" />
-				</a>
-			</div>
+	</div>
+	<div class="flex-1 center p-4">
+		<slot />
+	</div>
+	<!-- FOOTER -->
+	<div class="w-full hstack py-2 px-10 justify-center sm:justify-start footer">
+		<div class="hstack gap-5">
+			<a href="https://www.linkedin.com/in/dahyad/?originalSubdomain=uk" target="_blank">
+				<Icon icon={icons.linkedin} height="30px" />
+			</a>
+			<a href="https://www.instagram.com/dipi_d/?hl=en" target="_blank">
+				<Icon icon={icons.instagram} height="30px" />
+			</a>
+			<a href="https://www.tiktok.com/@dipzlikechipz_" target="_blank">
+				<Icon icon={icons.tiktok} height="30px" />
+			</a>
 		</div>
 	</div>
 </div>
