3464be7bb558cf1e107470bb8e31702d6b03b9ca
aiacuone <age.iacuone@aventus.io>
2023-09-14 01:16:22 +0200

    moved prevent default to on click

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -34,9 +34,11 @@
 		<button
 			use:shortLongPress
 			on:short-press={() => goto('/')}
-			on:long-press={(e) => {
-				e.preventDefault();
+			on:long-press={() => {
 				qrCodeModal.showModal();
+			}}
+			on:click={(e) => {
+				e.preventDefault();
 			}}
 			class="flex sm:hidden center self-center bg-white bg-opacity-10 py-1 px-2 rounded-md"
 		>
