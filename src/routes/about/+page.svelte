d0b5b689e6d7bebe277992f2db320ea702bbae50
aiacuone <age.iacuone@aventus.io>
2024-04-17 21:38:52 +0100

    dips changes

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,5 +1,5 @@
 <div class="stack gap-4">
-	<p>
+	<!-- <p>
 		<b>:DAID limited</b> specialises in a broad range of services with the founders qualified in the
 		following disciplines:
 	</p>
@@ -10,10 +10,28 @@
 		<li>Mechanical and Electrical Engineering</li>
 		<li>Digital Services and Business Transformation</li>
 	</ol>
-	<p>We are an equal opportunity employer and value diversity, equity and inclusion</p>
+	<p>We are an equal opportunity employer and value diversity, equity and inclusion</p> -->
+	<p>
+		:DAID limited’s vision is to revolutionise the consulting and software development industry by
+		utilising the latest technology and providing the first all in one business transformation
+		experience for clients. The founders are specialized in a broad range of services and qualified
+		in the following disciplines:
+	</p>
+	<ul>
+		<li>Accounting & Finance</li>
+		<li>Commercial Law and Taxation</li>
+		<li>Employment Relations</li>
+		<li>Mechanical and Electrical Engineering</li>
+		<li>Digital Services and Business Transformation</li>
+		<br />
+		<i>We are an equal opportunity employer and value diversity, equity and inclusion</i>
+	</ul>
 </div>
 
 <style>
+	li {
+		list-style-type: disc;
+	}
 	ol {
 		list-style-type: decimal;
 	}
